276cba219dded7a8d3ec55c436fa90becad3ff58
Jangchiyoung <clduddkwk@naver.com>
2021-10-05 13:07:11 +0900

    day12 13:07

--- src/com/jcpdev/action/FirstServlet.java
+++ src/com/jcpdev/action/FirstServlet.java
@@ -3,10 +3,12 @@ package com.jcpdev.action;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 //서블릿은 http 요청을 처리하는 자바 클래스입니다. HttpServlet 을 상속받습니다.
 public class FirstServlet extends HttpServlet{
@@ -26,6 +28,18 @@ public class FirstServlet extends HttpServlet{
 		out.print("<h3>JSP와 서블릿</h3>");
 		out.print("서블릿 너는 도대체 뭐냐? :");
 		
+		//현재 request 객체에 대한세션객체가 필요하다면
+		HttpSession session = request.getSession();
+		session.setAttribute("data", "first");
+		
+		//1) request 에 애트리뷰트 객체 저장(선택)
+		request.setAttribute("number", 123);
+		//pageContext.forward("first.jsp"); ->pageContext는 JSP 파일에서만 사용할 수 있는 내장 객체
+		//서블릿에서는 다음(?) 객체를 사용합니다.
+		//first.jsp(view 파일)가 현재의 request,response 전달 받을 수 있도록 감싸는 객체가 RequestDispatcher
+		//2)*** view 파일로 forward
+		RequestDispatcher rd = request.getRequestDispatcher("first.jsp");
+		rd.forward(request, response);
 	}
 	//요청 Method 가 Post일 때 처리되는 메소드 : 
 	@Override
